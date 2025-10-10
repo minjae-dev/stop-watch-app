f965a6969ac20bdafd31133fd04b4cbacb871e76
minjamie <minjae2246@gamil.com>
2025-10-09 18:52:14 -0700

    UI Header and Button section

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,82 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello, World!'),
-          centerTitle: false,
-          backgroundColor: Colors.blue,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 80),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey! Jamie',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withAlpha(204),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 120),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withAlpha(204),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  color: Colors.white.withAlpha(204),
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 20,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-        body: Center(child: Text('Welcome to Flutter!')),
+        backgroundColor: Color(0xFF181818),
       ),
     );
   }
